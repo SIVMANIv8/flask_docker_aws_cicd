ff09a18b7642e32cc326df757d16e83fe68690b3
Sivamani <cvaleads@live.in>
2024-01-17 13:38:02 +0530

    First commit of flask+docker+aws app

---
+++ app.py
@@ -0,0 +1,32 @@
+from flask import Flask, request, make_response, jsonify, json
+
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def index():
+    env = app.config['env']
+    #response.status_code = 404
+    #return make_response('',404)
+    return f"<h2>Helo Flask + Docker from <i>{env}</i> server<h2>"
+
+@app.route('/requirements', methods=['GET'])
+def requirements():
+     req_dct = {}
+     with open('requirements.txt', 'r') as req_list:
+          buff = req_list.read()
+          buff = buff.split('\n')
+          req_dct = {item.split('==')[0]:item.split('==')[1] for item in buff}
+          req_list.close()
+     rsp = app.response_class(
+         response = json.dumps(req_dct),
+         mimetype = 'application/json',
+         status = 200
+     )
+     return rsp
+
+if __name__ == '__main__':
+    app.config['env'] = 'Development'
+    app.run(host='0.0.0.0',port=8000, debug=True)
+else:
+    app.config['env'] = 'Production'
---
+++ tests/test_index.py
@@ -0,0 +1,12 @@
+from docker_aws_cicd.app import app
+import json
+
+def test_index_route():
+     response = app.test_client().get('/')
+     assert response.status_code == 200
+
+def test_requirement_route():
+     resp = app.test_client().get('/requirements')
+     assert resp.status_code == 200, "Invalid status response"
+     data = json.loads(resp.data.decode('utf-8'))
+     assert type(data) is dict,  "Invalid response content"
